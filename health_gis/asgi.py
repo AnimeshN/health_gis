59d369611f95766db0bbffe11849af7f9c5a7214
AnimeshN <animesh.nauti@gmail.com>
2020-03-28 15:57:29 +0530

    first-commit

---
+++ health_gis/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for health_gis project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_gis.settings')
+
+application = get_asgi_application()
