ce2f77e9efde4be0de7372aa42141efd339b2c81
AnimeshN <animesh.nauti@gmail.com>
2020-04-17 11:15:52 +0530

    mobile view

--- health_dash/urls.py
+++ health_dash/urls.py
@@ -3,12 +3,12 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    path('rural_health/', views.healthDash, name='rural_health'),
+    path('', views.healthDash, name='rural_health'),
     url(r'^login/', views.login_request, name='login'),
     url(r'^logout/', views.logout_request, name='logout'),
     url(r'^contribute/', views.contribute, name='contribute'),
     url(r'^urban_health/', views.UrbanHealth, name='urban_health'),
-    path('', views.trialDash, name='trail-dash'),
+    path('trail', views.trialDash, name='trail-dash'),
 
     
 ]
